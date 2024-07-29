ec777f22a75f51a964aa5e3cfdbb6fa46098ade6
Resty Galido <38154413+tyser1995@users.noreply.github.com>
2024-07-29 23:10:13 +0800

    Footer.tsx removed

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,1 @@
-"use client";
-export default function Footer() {
-  return (
-    <footer className="fixed bottom-0 left-0 w-full p-4">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
-        {/* Add your footer content here */}
-      </div>
-    </footer>
-  );
-}
+
